f1751cd9655a90e7b2f15786dbf3ddf58eab4731
Magus Dev <114507764+theMagusDev@users.noreply.github.com>
2022-11-05 16:35:28 +0300

    Create MarsApiService.kt, build retrofit, launch a coroutine in ViewModel to get the results

--- app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -2,7 +2,6 @@ package com.example.android.marsphotos.network
 
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
-import retrofit2.create
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com"
