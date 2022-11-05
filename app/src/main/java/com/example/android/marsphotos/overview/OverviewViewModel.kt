584b196025b1f56aee6dd91559df673841678d84
Magus Dev <114507764+theMagusDev@users.noreply.github.com>
2022-11-05 16:34:49 +0300

    Create MarsApiService.kt, build retrofit, launch a coroutine in ViewModel to get the results

--- app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
@@ -19,16 +19,17 @@ package com.example.android.marsphotos.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.android.marsphotos.network.MarsApi
+import kotlinx.coroutines.launch
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
  */
 class OverviewViewModel : ViewModel() {
 
-    // The internal MutableLiveData that stores the status of the most recent request
+    // the status of the most recent request
     private val _status = MutableLiveData<String>()
-
-    // The external immutable LiveData for the request status
     val status: LiveData<String> = _status
     /**
      * Call getMarsPhotos() on init so we can display status immediately.
@@ -42,6 +43,9 @@ class OverviewViewModel : ViewModel() {
      * [MarsPhoto] [List] [LiveData].
      */
     private fun getMarsPhotos() {
-        _status.value = "Set the Mars API status response here!"
+        viewModelScope.launch {
+            val listResult = MarsApi.retrofitService.getPhotos()
+            _status.value = listResult
+        }
     }
 }
